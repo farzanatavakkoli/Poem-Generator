e0d615723a8aac02a6d06ceb6e4f16bf75ecea94
Mohammad Reza Sabori <240224243+farzanatavakkoli@users.noreply.github.com>
2025-11-12 00:18:43 +0430

    added JavaScript and typewrite plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-
+function generatePoem(event) {
+  event.preventDefault();
+  new Typewriter("#poem", {
+    strings: "Do not go gentle into that good night,",
+    autoStart: true,
+    delay: 100,
+    cursor: "",
+  });
+}
+let poemGenerator = document.querySelector("#Poem-Generator");
+poemGenerator.addEventListener("submit", generatePoem);
