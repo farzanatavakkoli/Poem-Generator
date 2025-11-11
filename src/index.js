0bee86eb32f9e2d420d05292275fc2d3b9f513bf
Mohammad Reza Sabori <240224243+farzanatavakkoli@users.noreply.github.com>
2025-11-12 02:20:27 +0430

    Improved the User Experience

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 function displayPoem(response) {
   console.log("Poem generated");
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("loading");
+
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -10,17 +14,25 @@ function displayPoem(response) {
 
 function generatePoem(event) {
   event.preventDefault();
+
   let instructionInput = document.querySelector("#instruction-input");
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.classList.add("loading");
+
+  poemElement.innerHTML = `Generating poem about "${instructionInput.value}"...`;
+
   let apiKey = "25db17t7533c78o4da26332a44f6038e";
   let prompt = `User instruction: Generate a poem about ${instructionInput.value}`;
   let context =
-    "You are a high perfect poem expert and love to write short poems. Your mission is to generate 4 lines poem. Make sure to follow the user instructions and seperate each lines with a <br />. Sign the at the end of poem `AI Poem` and put it in the <strong> element";
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  console.log(`Generating Poem`);
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+    "You are a high perfect poem expert and love to write short poems. Your mission is to generate 4 lines poem. Make sure to follow the user instructions and separate each line with a <br />. Sign the poem at the end as <strong>AI Poem</strong>.";
 
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log(`Generating poem: ${instructionInput.value}`);
   axios.get(apiUrl).then(displayPoem);
 }
-let poemGenerator = document.querySelector("#Poem-Generator");
-poemGenerator.addEventListener("submit", generatePoem);
+
+document
+  .querySelector("#Poem-Generator")
+  .addEventListener("submit", generatePoem);
